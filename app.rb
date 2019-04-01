d880b718e20ea491c640df65ee2d9541fdff7e2f
parasquid <tristan.gomez@gmail.com>
2019-04-01 13:27:09 +0800

    add a timer to show how long the response takes

--- app.rb
+++ app.rb
@@ -13,10 +13,11 @@ LOCATION = settings.location
 get "/" do
   client = Stream::Client.new(APIKEY, SECRET, APPID, { location: LOCATION })
 
-  user_feed = client.feed('user', '7')
+  @user_id = 7
+  user_feed = client.feed('user', @user_id)
   @user_token = user_feed.readonly_token
 
-  notification_feed = client.feed('notification', '7')
+  notification_feed = client.feed('notification', @user_id)
   @notification_token = notification_feed.readonly_token
 
   @apikey = APIKEY
@@ -26,19 +27,29 @@ end
 
 get "/add_activity" do
   client = Stream::Client.new(APIKEY, SECRET, APPID, { location: LOCATION })
-  feed = client.feed('user', '7')
+  @user_id = 7
 
   activity_data = {
-    actor: "7",
+    actor: @user_id,
     verb: "test",
     object: 0,
   }
 
-  activity_response = feed.add_activity(activity_data)
+  user_feed = client.feed('user', @user_id)
+  notification_feed = client.feed('notification', @user_id)
+
+  user_activity_response = user_feed.add_activity(activity_data)
+  notification_activity_response = notification_feed.add_activity(activity_data)
 
   content_type :json
 
   @apikey = APIKEY
   @appid = APPID
-  { success: true, data: activity_response }
+  {
+    success: true,
+    data: {
+      user: user_activity_response,
+      notification: notification_activity_response,
+    }
+  }.to_json
 end
